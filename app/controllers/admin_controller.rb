6f405e99a5504e639fc8199d2051255344c3fd49
Fabio Santos <fabiosantos.pe@gmail.com>
2020-08-30 20:30:40 -0300

    fix cancancan create error

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,8 @@
 class AdminController < ApplicationController
     layout 'admin'
-    load_and_authorize_resource
+    load_and_authorize_resource unless: :is_admin?
+
+    def is_admin?
+        current_user.admin?
+    end
 end
