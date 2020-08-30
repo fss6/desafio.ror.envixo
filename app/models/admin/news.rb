74c31d4f158d87035aaa162448c2740d59201599
Fabio Santos <fabiosantos.pe@gmail.com>
2020-08-30 16:17:33 -0300

    Update index news

--- app/models/admin/news.rb
+++ app/models/admin/news.rb
@@ -5,6 +5,20 @@ class Admin::News < ApplicationRecord
                           foreign_key: :admin_news_id, 
                           association_foreign_key: :admin_tag_id
   accepts_nested_attributes_for :tags, reject_if: proc { |id| id.blank? }
+  has_many :comments, as: :commentable
+  accepts_nested_attributes_for :comments, allow_destroy: true
+  has_many :scores, as: :scoreable
+  accepts_nested_attributes_for :scores, allow_destroy: true
 
   validates :title, :subtitle, :content, presence: true
+
+  def verified_comments(locale)
+    comments.where(verified: true, locale: locale).order(created_at: :asc) 
+  end
+
+  def points
+    total_values = self.scores.sum(:value)
+    total_evaluations = scores.count
+    total_evaluations.zero? ? 0 : (total_values / total_evaluations.to_f).round
+  end 
 end
