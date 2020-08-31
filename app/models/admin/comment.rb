e40e4628f114e59e24735a7181124dbd1532ac7f
Fabio Santos <fabiosantos.pe@gmail.com>
2020-08-31 12:39:07 -0300

    add style to notice alert

--- app/models/admin/comment.rb
+++ app/models/admin/comment.rb
@@ -6,4 +6,9 @@ class Admin::Comment < ApplicationRecord
   def locale_human
     I18n.t(:language, locale: self[:locale])
   end
+
+  def commentable_translate_by_class_name
+    commentable_class_name = commentable.class.name.split('::').last.downcase rescue nil
+    I18n.t("activerecord.models.#{commentable_class_name}.one") rescue ''
+  end
 end
