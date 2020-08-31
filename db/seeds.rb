e00f75ef97daff70689251c34aeab5998b275b8f
Fabio Santos <fabiosantos.pe@gmail.com>
2020-08-31 12:43:28 -0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,28 +11,26 @@ Faker::Config.locale = :pt
 User.create(name: "Administrator", email: "admin@admin.com", password: "admin@123", admin: true)
 User.create(name: "User", email: "user@user.com", password: "user@123")
 
-10.times do |i|
+5.times do |i|
     Admin::Tag.create(name: Faker::Lorem.word.upcase, locale: :pt, user: User.first)
     Admin::Tag.create(name: Faker::Lorem.word.upcase, locale: :es, user: User.first)
 end
 
-10.times do |i|
+5.times do |i|
     video = Admin::Video.new do |v|
         v.title = Faker::Movie.title
         v.url = 'https://www.youtube.com/embed/zpOULjyy-n8?rel=0'
         v.user = User.first
     end
-    video.tags << Admin::Tag.first
     video.save
 end
 
-10.times do |i|
+5.times do |i|
     news = Admin::News.new do |n|
         n.title = Faker::Lorem.sentence
         n.subtitle = Faker::Lorem.sentence
         n.content = Faker::Lorem.paragraph
         n.user = User.first
     end
-    news.tags << Admin::Tag.first
     news.save
 end
